73084f926652a9d6f8ce0decb2b285bb272e6d0b
bala-amypo <bala@amypo.in>
2025-12-18 04:15:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -15,38 +15,38 @@ public class StudentEntity{
     private String name;
     private String email;
     private String password;
-                            private Date createAt;
+    private Date createAt;
 
-                                public void setId(Integer id){
-                                        this.id = id;
-                                            }
-                                                public Integer getId(){
-                                                        return id;
-                                                            }
-                                                                public String getName(){
-                                                                        return name;
-                                                                            }
-                                                                                public void setName(String name){
-                                                                                        this.name = name;
-                                                                                            }
-                                                                                                public String getEmail(){
-                                                                                                        return email;
-                                                                                                            }
-                                                                                                                public void setEmail(String email){
-                                                                                                                        this.email = email;
-                                                                                                                            }
-                                                                                                                                public String getPassword(){
-                                                                                                                                        return password;
-                                                                                                                                            }
-                                                                                                                                                public void setPassword(String password){
-                                                                                                                                                        this.password = password;
-                                                                                                                                                            }
-                                                                                                                                                                public Date getCreateAt(){
-                                                                                                                                                                        return createAt;
-                                                                                                                                                                            }
-                                                                                                                                                                                public void setCreateAt(Date createAt){
-                                                                                                                                                                                        this.createAt = createAt;
-                                                                                                                                                                                            }
+    public void setId(Integer id){
+        this.id = id;
+        }
+    public Integer getId(){
+        return id;
+        }
+    public String getName(){
+        return name;
+        }
+    public void setName(String name){
+        this.name = name;
+        }
+    public String getEmail(){
+        return email;
+        }
+    public void setEmail(String email){
+        this.email = email;
+        }
+    public String getPassword(){
+        return password;
+        }
+    public void setPassword(String password){
+        this.password = password;
+        }
+    public Date getCreateAt(){
+        return createAt;         
+        }
+    public void setCreateAt(Date createAt){
+        this.createAt = createAt;
+                                                                                                                                        }
                                                                                                                                                                                                 public void StudentEntity(){
 
                                                                                                                                                                                                     }
