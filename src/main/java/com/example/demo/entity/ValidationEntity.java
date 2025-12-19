80d80cacb332c6992eb48de4d66cdeb3ed207d0a
bala-amypo <bala@amypo.in>
2025-12-19 06:36:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -14,7 +14,7 @@ import com.example.demo.service.ValidationService;
 import jakarta.validation.Valid;
 import lombok.Data;
 import lombok.AllArgsConstructor;
-import lombok.
+import lombok.NoArgsConstructor;
 
 
 @Entity
