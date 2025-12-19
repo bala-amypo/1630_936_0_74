d08da5fba1028e61b20ac6a123bf6d05936967db
bala-amypo <bala@amypo.in>
2025-12-19 08:03:14 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampImpl.java
@@ -2,17 +2,18 @@ package com.example.demo.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service; 
-import com.example.demo.entity.ValidationEntity;
-import com.example.demo.repository.ValidationRepository;
+import com.example.demo.entity.TimeStamp;
+import com.example.demo.repository.TimeStampRepository;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.example.demo.service.ValidationService;  
-import com.example.demo.exception.ValidationException;
+import com.example.demo.service.TimeStampService;  
+import com.example.demo.exception.TimeStampException;
 
 
 @Service
-public class TimeStampImpl implements TService{
-    @Autowired ValidationRepository valid;
+public class TimeStampImpl implements TimeStampService{
+    @Autowired TimeStampRepository valid;
     @Override
-    public ValidationEntity postdata(ValidationEntity val){
+    public TimeStamp postdata(TimeStamp val){
         return valid.save(val);  
     }
+}
